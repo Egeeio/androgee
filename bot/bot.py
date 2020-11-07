b8a5adca78fd6aa0804c05825ddfb4f395d407bb
egee-irl <brian@egee.io>
2020-11-07 15:42:52 -0800

    boilerplate

---
+++ bot/bot.py
@@ -0,0 +1,18 @@
+import os
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+token = os.environ['DISCORD_TOKEN']
+client.run(token)
