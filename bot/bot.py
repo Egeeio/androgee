d40a16a58d10b7debb68aa3fbc375ee7b71afa4c
advaithm <advaith.madhukar@gmail.com>
2020-11-08 08:26:53 +0530

    reworked to use commands.Bot()+f-strings and added help,nuzzle commands

--- bot/bot.py
+++ bot/bot.py
@@ -1,14 +1,39 @@
 import os
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot("$")
 
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f"We have logged in as {client.user}")
 
 
+# command = $hello
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello!")
+
+
+# command = $nuzzle
+@client.command()
+async def nuzzle(ctx):
+    await ctx.send(f"*nuzzles* {ctx.author.name}")
+
+
+# help is a inbuilt function it will raise an error when used
+@client.command()
+async def h(ctx):
+    message = """
+    **$hello** get Hello back
+**$nuzzle** get nuzzled
+**$h**   get this message
+    """
+    await ctx.send(message)
+
+
+"""
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -16,5 +41,6 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
-token = os.environ['DISCORD_TOKEN']
+"""
+token = os.environ["DISCORD_TOKEN"]
 client.run(token)
