1acf20ca1d589972a69875ed4dfcbc6a370786f1
egee-irl <brian@egee.io>
2020-11-07 16:06:29 -0800

    linting

--- bot/bot.py
+++ bot/bot.py
@@ -3,9 +3,11 @@ import discord
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
