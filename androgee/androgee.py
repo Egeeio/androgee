9793a7b02a359dba8e4d7846918db5641a5a1202
egee-irl <brian@egee.io>
2020-11-24 10:12:29 -0800

    updated project

--- androgee/androgee.py
+++ androgee/androgee.py
@@ -1,28 +1,38 @@
 import os
+import random
 import logging
+import discord
 from discord.ext import commands
 
 
-logging.basicConfig(level=logging.INFO)
-
-bot = commands.Bot(command_prefix='$')
+logging.basicConfig(level=logging.WARNING)
+bot = commands.Bot(command_prefix=os.environ['DISCORD_PREFIX'])
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print('We is logged in as {0.user}'.format(bot))
 
 
 @bot.command(name="spray")
 async def spray(ctx):
-    await jail(ctx)
+    image = get_image(ctx)
+    await ctx.send(file=image)
 
 
 @bot.command(name="bonk")
 async def spray(ctx):
-    jail(ctx)
+    image = get_image(ctx)
+    await ctx.send(file=image)
 
-async def jail(ctx):
-    await ctx.send(f"bonking {ctx.author.name}")
 
-token = os.environ['DISCORD_TOKEN']
-bot.run(token)
+def get_image(ctx):
+    images = []
+    files = os.walk(f"{os.path.dirname(__file__)}/media/{ctx.command}")
+    for root, dirs, files in files:
+        for name in files:
+            images.append(f"{root}/{name}")
+    img = discord.File(random.choice(images))
+    return img
+
+def start():
+    bot.run(os.environ['DISCORD_TOKEN'])
