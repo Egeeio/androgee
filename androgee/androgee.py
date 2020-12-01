3ebdfee1e0ea30293feccad5f56188e632c9ceec
advaithm <advaith.madhukar@gmail.com>
2020-12-01 13:13:30 +0530

    added code for autorole, on a side note what is life?

--- androgee/androgee.py
+++ androgee/androgee.py
@@ -2,11 +2,18 @@ import os
 import random
 import logging
 import discord
+from discord.utils import get
 from discord.ext import commands
 
 
 logging.basicConfig(level=logging.WARNING)
 bot = commands.Bot(command_prefix=os.environ["DISCORD_PREFIX"])
+global black_list
+black_list = []
+with open("black_list.txt", "r") as f:
+    for line in f.readlines():
+        black_list.append(line.strip())
+    f.close()
 
 
 @bot.event
@@ -32,6 +39,35 @@ async def spray(ctx, member: discord.Member = None):
     else:
         message = f"{member.mention} was bonked by {ctx.message.author.mention}"
         await ctx.send(message, file=image)
+
+
+@bot.command("role")
+async def role(ctx, command, *, role=None):
+    if command == "regen":
+        global black_list
+        black_list = []
+        with open("black_list.txt", "r") as f:
+            for line in f.readlines():
+                black_list.append(line.strip())
+        f.close()
+    elif command == "list":
+        roles = await ctx.guild.fetch_roles()
+        role_list = ""
+        for role in roles:
+            if role.name in black_list:
+                pass
+            else:
+                role_list = role_list + role.name + "\n"
+        await ctx.send(f"these are the avalible roles:\n{role_list}")
+    elif command == "add":
+        if role == None:
+            await ctx.send("must specify role.")
+        else:
+            if role in black_list:
+                await ctx.send("only a admin can assign this role")
+            else:
+                role_to_give = get(ctx.guild.roles, name=role)
+                await ctx.author.add_roles(ctx.author, role_to_give)
 
 
 def get_image(ctx):
