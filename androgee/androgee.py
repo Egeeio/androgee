3e54ff57e714a78ef780206f6f4364639da4765c
advaithm <advaith.madhukar@gmail.com>
2020-12-24 10:03:13 +0530

    updated to mee6

--- androgee/androgee.py
+++ androgee/androgee.py
@@ -1,7 +1,6 @@
 import os
 import random
 import logging
-import asyncio
 import discord
 from discord.ext import commands
 
@@ -16,20 +15,20 @@ async def on_ready():
     print(f"We is logged in as {bot.user}")
 
 
-@bot.command(name="spray")
+@bot.command(name="spray", aliases=["spritzered"])
 async def spray(ctx, member: discord.Member = None):
     image = get_image(ctx)
     if member == None:
         await ctx.send(file=image)
     else:
-        if mod_role_id in [y.id for y in ctx.author.roles]:
-            await member.edit(mute=True)
+        ctx.send(f"!tempmute {member.mention} 300 being too rowdy")
+        if "spray" in ctx.message.content:
             message = f"{member.mention} was sprayed by {ctx.message.author.mention}"
-            await ctx.send(message, file=image)
-            await asyncio.sleep(10)
-            await member.edit(mute=False)
         else:
-            message = f"{member.mention} was sprayed by {ctx.message.author.mention}"
+            message = f"{member.mention} was sprirzered by {ctx.message.author.mention}"
+        if mod_role_id in [y.id for y in ctx.author.roles]:
+            await ctx.send(message, file=image)
+        else:
             await ctx.send(message, file=image)
 
 
@@ -40,11 +39,9 @@ async def spray(ctx, member: discord.Member = None):
         await ctx.send(file=image)
     else:
         if mod_role_id in [y.id for y in ctx.author.roles]:
-            await member.edit(mute=True)
+            ctx.send(f"!tempmute {member.mention} 300 being too rowdy")
             message = f"{member.mention} was bonked by {ctx.message.author.mention}"
             await ctx.send(message, file=image)
-            await asyncio.sleep(10)
-            await member.edit(mute=False)
         else:
             message = f"{member.mention} was bonked by {ctx.message.author.mention}"
             await ctx.send(message, file=image)
@@ -53,7 +50,7 @@ async def spray(ctx, member: discord.Member = None):
 @bot.command(name="source")
 async def source(ctx):
     message = (
-        f"{ctx.author.mention} the source is at https://github.com/Egeeio/androgees"
+        f"{ctx.author.mention} the source is at https://github.com/Egeeio/androgee"
     )
     await ctx.send(message)
 
