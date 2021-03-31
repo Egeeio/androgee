8444903d2a0ef903722b74781f51b940b38ae9c0
Deeryeen <husky@deeryeen.com>
2021-03-31 02:01:12 +0200

    + Added env variables verification and assignment Androgee now quits when they can't find the right env vars.

--- androgee/androgee.py
+++ androgee/androgee.py
@@ -1,14 +1,23 @@
 import os
+import sys
 import random
 import logging
 import discord
 from discord.ext import commands
 
+# I think it'd be better if we check if environment
+# variables are present before doing anything else.
+try:
+    mod_role_id = os.environ["mod_role_id"]
+    mod_role_name = os.environ["mod_role_name"]
+    COMMAND_PREFIX = os.environ["DISCORD_PREFIX"]
+    BOT_TOKEN = os.environ["DISCORD_TOKEN"]
+except KeyError as e:
+    print(f'The {e} environment variable is missing! Androgee cannot run!')
+    sys.exit(1)
 
 logging.basicConfig(level=logging.WARNING)
-bot = commands.Bot(command_prefix=os.environ["DISCORD_PREFIX"])
-mod_role_id = os.environ["mod_role_id"]
-mod_role_name = os.environ["mod_role_name"]
+bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
 
 @bot.event
@@ -76,4 +85,4 @@ def get_image(ctx):
 
 
 def start():
-    bot.run(os.environ["DISCORD_TOKEN"])
+    bot.run(BOT_TOKEN)
